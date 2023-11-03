09dd6e418b8632e884a207fc2b0ccdce58abc2e7
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2023-11-03 20:42:20 +0000

    add all js files with comments, create array containing all questions & answers

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// create an array of objects to store each question and answer data
+var questions = [
+     {
+        q: "Which operator is used for strict equality comparison in JavaScript?",
+        a: ["==","===","=","!="],
+        aIndex: 1
+    },
+    {
+        q: "Which method is used to add an element to the end of an array in JavaScript?",
+        a: ["append()","push()","add()","insert()"],
+        aIndex: 1
+    },
+    {
+        q: "How do you declare a function in JavaScript?",
+        a: ["function myFunction()","declare myFunction()","create function myFunction()","function = myFunction()"],
+        aIndex: 0
+    },
+    {
+        q: "Which of the following is not a JavaScript data type?",
+        a: ["string","number","boolean","character"],
+        aIndex: 3
+    },
+    {
+        q: "Which built-in function is used to print something to the console in JavaScript?",
+        a: ["write()","print()","log()","display()"],
+        aIndex: 2
+    }
+]
---
+++ assets/js/scores.js
@@ -0,0 +1,6 @@
+// This page connects to the highscores.html
+
+//retrieve all the scores from local storage
+// order the scores from highest to lowest
+// create a list item for each score and display it to the screen
+// ensure the "clear scores" button works
